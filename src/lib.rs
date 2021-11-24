0f87150fd8e8437ed7768d573bfac5135d5cbdee
Douglas Marodin <douglasmarodin@gmail.com>
2021-11-23 22:58:00 -0300

    Moved event loop to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,32 @@
 use wasm_bindgen::prelude::*;
+use winit::event::Event;
+use winit::dpi::PhysicalSize;
 
 mod plataform;
 
 #[wasm_bindgen(start)]
 pub fn main() {
-    plataform::init();
+    let plataform_state = plataform::init();
+
+    plataform_state
+        .window_state
+        .event_loop
+        .run(move |event, _, _| {
+            let body = plataform_state.window_state.document.body().unwrap();
+            let size = PhysicalSize {width: body.client_width() as u32, height: body.client_height() as u32};
+            if plataform_state.window_state.window.inner_size() != size {
+                plataform_state.window_state.window.set_inner_size(size);
+            }
+
+            match event {
+                Event::WindowEvent {
+                    ref event,
+                    window_id,
+                } if window_id == plataform_state.window_state.window.id() => match event {
+                    _ => {}
+                },
+                _ => {}
+            }
+        });
 }
 
--- src/plataform/plataform.rs
+++ src/plataform/plataform.rs
@@ -5,7 +5,7 @@ use window::WindowState;
 use winit::dpi::PhysicalSize;
 use winit::event::*;
 
-struct PlataformState {
+pub struct PlataformState {
     pub window_state: WindowState,
 }
 
@@ -17,28 +17,8 @@ impl PlataformState {
     }
 }
 
-pub fn init() {
+pub fn init() -> PlataformState {
     logging::prep_logging();
     let plataform_state = PlataformState::new();
-
     plataform_state
-        .window_state
-        .event_loop
-        .run(move |event, _, _| {
-            let body = plataform_state.window_state.document.body().unwrap();
-            let size = PhysicalSize {width: body.client_width() as u32, height: body.client_height() as u32};
-            if plataform_state.window_state.window.inner_size() != size {
-                plataform_state.window_state.window.set_inner_size(size);
-            }
-
-            match event {
-                Event::WindowEvent {
-                    ref event,
-                    window_id,
-                } if window_id == plataform_state.window_state.window.id() => match event {
-                    _ => {}
-                },
-                _ => {}
-            }
-        });
 }
